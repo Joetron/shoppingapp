eda75847f64fd45417203a245a11fafd940574d4
joetron <joetron@hotmail.com>
2021-01-22 11:56:19 -0800

    move core code to src folder

---
+++ containers/ProductsList copy/index.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import ProductsList from '../../components/ProductsList';
+import { addToCart, removeFromCart } from '../../src/actions';
+
+const mapDispatchToProps = (dispatch) => ({
+  onAddToCart: (product) => dispatch(addToCart(product)),
+  onRemoveFromCart: (product) => dispatch(removeFromCart(product)),
+});
+
+const mapStateToProps = (state) => ({
+  products: state.products,
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductsList);
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { AppRegistry } from 'react-native';
-import App from './containers/App';
+import App from './src/containers/App';
 import { name as appName } from './app.json';
-import store from './store';
+import store from './src/store';
 
 const ConnectedApp = () => (
   <Provider store={store}>
